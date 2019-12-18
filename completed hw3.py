0776bf3635a4a00814cc978db29816394ca4d96e
kirillr123 <wannabefree232@gmail.com>
2019-12-18 20:59:57 +0300

    Fix

--- completed hw3.py
+++ completed hw3.py
@@ -1,20 +1,9 @@
-"""
-Первым циклом проходимся по листу введенных данных и записываем слова и количества повторов в словарь, вторым считываем
-"""
 while True:
     print("Введите текст...")
     a = input()
     words = a.split()
-    uwords = dict()
-    maxcount = 0
-    for word in words:
-        if not uwords.__contains__(word.lower()):
-            uwords[word.lower()] = 1
-        else:
-            uwords[word.lower()] = uwords[word.lower()] + 1
-            if maxcount < uwords[word.lower()]:
-                maxcount = uwords[word.lower()]
-    for word in uwords:
-        if uwords[word] == maxcount:
-            print(uwords[word], " - ", word)
-
+    words = [word.lower() for word in words]
+    uwords = {word:words.count(word) for word in words}
+    final = {word:uwords[word] for word in uwords if uwords[word] == max(uwords.values())}
+    for word in final:
+        print(final[word], " - ", word)
