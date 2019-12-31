33804bff80458b200695299a0f2e53133be0cb56
kirillr123 <wannabefree232@gmail.com>
2019-12-31 03:17:45 +0300

    fix

--- completed hw 9(problem 40).py
+++ completed hw 9(problem 40).py
@@ -1,2 +1,4 @@
 import functools    # долго считает зато в одну строку!
-print(functools.reduce(lambda a, b: a * b, [int(functools.reduce(lambda a, b: str(a) + str(b), [x for x in range(1, 200000)], '')[10 ** a - 1]) for a in range(7)]))
+print('Project Euler problem 40 solution:', functools.reduce(lambda a, b: a * b,
+[int(functools.reduce(lambda a, b: str(a) + str(b), [x for x in range(1, 200000)], '')[10 ** a - 1]) for a in range(7)]))
+
--- completed hw4.py
+++ completed hw4.py
@@ -3,6 +3,4 @@ import functools
 print("Введите целые неотрицательные числа, разделенные любым не цифровым литералом (пробел, запятая, %, буква и т.д.).")
 a = input()
 p = re.compile(r'-?[0-9]+')
-print(functools.reduce(lambda a, b: int(a)+int(b), p.findall(a)))
-print("Second solution")
 print(sum(int(e) for e in p.findall(a)))
