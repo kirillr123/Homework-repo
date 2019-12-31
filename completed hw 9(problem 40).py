9bb9f66d0442628897c7cc84873fa56e136fc21f
kirillr123 <wannabefree232@gmail.com>
2019-12-31 03:19:29 +0300

    fix

--- completed hw 9(problem 40).py
+++ completed hw 9(problem 40).py
@@ -1,4 +1,3 @@
 import functools    # долго считает зато в одну строку!
 print('Project Euler problem 40 solution:', functools.reduce(lambda a, b: a * b,
 [int(functools.reduce(lambda a, b: str(a) + str(b), [x for x in range(1, 200000)], '')[10 ** a - 1]) for a in range(7)]))
-
